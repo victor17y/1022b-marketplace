211ce39cd09caba618176d2aaedd46840298f134
Victor Hugo <118356177+victor17y@users.noreply.github.com>
2024-09-30 08:24:54 -0400

    Atualizaçao para hospedagem

---
+++ src/main.ts
@@ -0,0 +1,36 @@
+import express from 'express'
+import cors from 'cors'
+import mysql from 'mysql2/promise'
+
+//criar um obijeto do tipo express.
+const app = express ()
+//incluir pra ele receber json
+app.use(express.json())
+//incluir o cors-> quando a gente tem outra porta faz 
+app.use(cors())
+//rotas
+app.get("/produtos",async(req,res)=>{
+try{ 
+    const conexao = await mysql.createConnection({
+        host:process.env.dbhost?process.env.dbhost:"localhost",
+
+        database:process.env.dbname?process.env.dbname:"banco1022b",
+
+        user:process.env.dbuser?process.env.dbuser:"root",
+
+        password:process.env.dbpassword?process.env.dbpassword:"",
+
+        port:process.env.dbport?parseInt(process.env.dbport):3306,
+
+    })
+    const [result,filds]=await conexao.query("select*from produtos")
+
+    res.send(result)}
+catch(e){
+    res.status(500).send("erro do servidor")
+}
+})
+//iniciar o servidor
+app.listen (8000,()=>{
+    console.log('servidor iniciado na porta 8000')
+})
